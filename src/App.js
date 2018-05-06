b0945e8686e0dfafe708642b85f5f6fde9681f6c
yang <89915256@qq.com>
2018-05-06 22:32:05 +0800

    前后联调-1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { addGun, removeGun, addGunAsync } from './index.redux'
 
 @connect(
 	// 你要state什么属性放到paops里
-	state => ({num: state}),
+	state => ({num: state.counter}),
 	// 你要什么方法， 放到props里，自动dispatch
 	{ addGun, removeGun, addGunAsync }
 )
--- src/index.js
+++ src/index.js
@@ -14,49 +14,22 @@ import {
 
 import App from './App'
 import { counter } from './index.redux'
+import Auth from './Auth'
+import reducers from './reducer'
 
 const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : () => {}
-const store = createStore(counter, compose(
+const store = createStore(reducers, compose(
 	applyMiddleware(thunk),
 	reduxDevtools
 ))
 
-function Erying () {
-	return <h2>二营</h2>
-}
-
-function Qibinglian () {
-	return <h2>骑兵连</h2>
-}
-
-class Test extends React.Component {
-	render () {
-		console.log(this.props)
-		return <h2>404 {this.props.location.pathname}</h2>
-	}
-}
 
 ReactDom.render(
 	(<Provider store={store}>
 		<BrowserRouter>
 			<div>
-				<ul>
-					<li>
-						<Link to='/'>一营</Link>
-					</li>
-					<li>
-						<Link to='/erying'>二营</Link>
-					</li>
-					<li>
-						<Link to='/qibinglian'>骑兵连</Link>
-					</li>
-				</ul>
-				<Switch>
 					<Route path='/' exact component={App}></Route>
-					<Route path='/erying' component={Erying}></Route>
-					<Route path='/qibinglian' component={Qibinglian}></Route>
-					<Route path='/:location' component={Test}></Route>
-				</Switch>
+					<Route path='/login' component={Auth}></Route>
 			</div>
 		</BrowserRouter>
 	</Provider>),
